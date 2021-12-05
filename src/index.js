1bc25a44c758e54fec63830cee6a92a2725f9fe2
leezer94 <2kunhee94@gmail.com>
2021-12-06 02:23:09 +0900

    feat: 버튼 이벤트 리스너 생성 1. 자동차 입력값 버튼 이벤트 생성 2. 레이싱 횟수 버튼 이벤트 생성

--- src/index.js
+++ src/index.js
@@ -4,8 +4,22 @@ import {
 } from './view/display.js';
 
 const init = function () {
+  // Selectors
+  const carNamesButton = document.querySelector('#car-names-submit');
+  const racingCountButton = document.querySelector('#racing-count-submit');
+
+  // Functions
   hideRacingCountContainer();
   hideResultContainer();
+
+  // EventListeners
+  carNamesButton.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+
+  racingCountButton.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
 };
 
 init();
