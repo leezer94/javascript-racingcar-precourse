2cd4385a909e92f6f08b184c21e84356243a6e7c
leezer94 <2kunhee94@gmail.com>
2021-12-06 16:39:08 +0900

    feat: 레이싱 카운트 입력값 유효성 검사후의 기능 구현 1. 핸들러 함수안, #result 텍스트 보여주는 기능 구현 2. 유효성 검사 통과후 결과 값 출력하는 엘리먼트 생성

--- src/handlers.js
+++ src/handlers.js
@@ -1,14 +1,32 @@
-import { validatedCarNames } from './validator.js';
-import { showRacingCountDiv } from './view/display.js';
-import { getCarNamesInput } from './view/get-user-input.js';
+import { makeNewCars } from './game-utils.js';
+import { validatedCarNames, validateRacingCount } from './validator.js';
+import {
+  appendResultSpanElement,
+  showRacingCountDiv,
+  showResultDiv,
+} from './view/display.js';
+import {
+  getCarNamesInput,
+  getRacingCountInput,
+} from './view/get-user-input.js';
 
 export const handleCarNamesSubmitButton = function () {
   const carNamesInputValue = getCarNamesInput();
   const carNamesArray = carNamesInputValue.split(',');
 
   if (validatedCarNames(carNamesArray)) {
+    makeNewCars(carNamesArray);
     showRacingCountDiv();
   }
 
   return true;
+};
+
+export const handleRacingCountSubmitButton = function () {
+  const racingCount = getRacingCountInput();
+
+  if (validateRacingCount(racingCount)) {
+    showResultDiv();
+    appendResultSpanElement();
+  }
 };
